5acd4e87ab99ca53e3e16b5c015ec42b1249a12c
Rodion Kokotkin <106533198+r0keee@users.noreply.github.com>
2025-05-23 14:55:06 +0300

    Update std_lib_test.cpp

--- std_lib_test.cpp
+++ std_lib_test.cpp
@@ -18,3 +18,20 @@ TEST(StandartLibSuite, NumberTests) {
     ASSERT_TRUE(output.str()[0] == '4' && output.str()[1] == '2');
     ASSERT_TRUE(output.str()[2] == '0' || output.str()[2] == '1' || output.str()[2] == '2' || output.str()[2] == '3');
 }
+
+TEST(OperationsSuite, StrAndListLen) {
+    std::string code = R"(
+        x = "veniamin"
+        y = [1, 3, 3, 7]
+        print(len(x))
+        print(len(y))
+    )";
+
+    std::string expected = "84";
+
+    std::istringstream input(code);
+    std::ostringstream output;
+
+    ASSERT_TRUE(interpret(input, output));
+    ASSERT_EQ(output.str(), expected);
+}
